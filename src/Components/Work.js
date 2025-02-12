5030db4c0b76b0f9f5440610fd60d7fd1e7346dc
constance khalumba <constancekhalumba@gmail.com>
2025-02-12 12:36:17 +0300

    first commit

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,34 +1,39 @@
 import React from "react";
-import PickMeals from "../Assets/pick-meals-image.png";
-import ChooseMeals from "../Assets/choose-image.png";
-import DeliveryMeals from "../Assets/delivery-image.png";
+import PickMeals from "../Assets/balanced-diet.png";
+import ChooseMeals from "../Assets/navigator.png";
+import DeliveryMeals from "../Assets/bell.png";
+import ImpactTrack from "../Assets/bar-graph.png";
 
 const Work = () => {
   const workInfoData = [
     {
       image: PickMeals,
-      title: "Pick Meals",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+      title: "Food Donation Matching",
+      text: " Connects food donors with local organizations and individuals in need, ensuring surplus food is distributed to those who need it most.",
     },
     {
       image: ChooseMeals,
-      title: "Choose How Often",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+      title: "Real-Time Location-Based Matching",
+      text: "Uses location services to help users find the nearest food distribution points and donation centers quickly and efficiently.",
     },
     {
       image: DeliveryMeals,
-      title: "Fast Deliveries",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+      title: "Instant Notifications",
+      text: "Sends real-time alerts about available food donations, pickup schedules, and important updates to keep users informed.",
+    },
+    {
+      image: ImpactTrack,
+      title: "Impact Tracking",
+      text: "Provides insights on the amount of food saved, meals distributed, and the overall impact on the community through data reports.",
     },
   ];
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <p className="primary-subheading">Work</p>
-        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-subheading">Features</p>
+        <h1 className="primary-heading">Description</h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        Redifu makes food sharing easy and efficient by connecting donors with those in need. Our platform ensures seamless coordination, real-time updates, and impact tracking to reduce food waste and fight hunger.
         </p>
       </div>
       <div className="work-section-bottom">
