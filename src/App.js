48af1104f33b9a1a6872437935673ca7d1aea89e
constance khalumba <constancekhalumba@gmail.com>
2025-02-22 17:55:33 +0300

    saturday commits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
